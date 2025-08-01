9966bbc69f8bd1485f5fa3f26351dc9aad9071c6
inulpro <inulpro88@gmail.com>
2025-08-01 23:26:41 +0700

    database setup

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,8 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold text-rose-500">
-        <Button>Click me</Button>
-      </h1>
+      <h1 className="text-3xl font-bold text-rose-500">Hello World</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
